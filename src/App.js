e0098fc0c44aff36970b8a20d95e2c3772565c19
Alewi Ahmed <alewimail@gmail.com>
2018-05-10 15:44:59 +0300

    Initial Commit - installed bootstrap & react-spring

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+    return <div className="app-container">Empty project</div>;
+  }
+}
+
+export default App;
