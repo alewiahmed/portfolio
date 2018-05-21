29989f2cb7b3847e0a325121020582a2b3664393
Alewi Ahmed <alewimail@gmail.com>
2018-05-21 14:59:12 +0300

    Removed index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import fontawesome from '@fortawesome/fontawesome';
 import brands from '@fortawesome/fontawesome-free-brands';
